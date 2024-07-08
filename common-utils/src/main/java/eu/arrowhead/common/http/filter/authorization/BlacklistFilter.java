00c8ebfd5e9abc5814bb3615e9dd8f5ecbf891c2
Rajmund Bocsi <rbocsi@aitia.ai>
2024-07-08 09:46:45 +0200

    new planned feature in blacklist service

--- common-utils/src/main/java/eu/arrowhead/common/http/filter/authorization/BlacklistFilter.java
+++ common-utils/src/main/java/eu/arrowhead/common/http/filter/authorization/BlacklistFilter.java
@@ -23,13 +23,14 @@ public class BlacklistFilter extends ArrowheadFilter {
 	@Override
 	protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain) throws IOException, ServletException {
 		try {
-			
+
 			Object systemName = request.getAttribute(Constants.HTTP_ATTR_ARROWHEAD_AUTHENTICATED_SYSTEM);
 
 			// TODO this filter should lookup for the blacklist service and use it against to the system name
 			// if system name is found on the blacklist throw ForbiddenException
 			// if requester is sysop, no need for check
 			// if request is for lookup for authentication, no need for check
+			// blacklist entry can be temporary (with expiration)
 
 			chain.doFilter(request, response);
 		} catch (final ArrowheadException ex) {
