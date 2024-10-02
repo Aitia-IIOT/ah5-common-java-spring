17ff00045b4252d5f3afe32493793b3ad851a06c
borditamas <borditamas@gmail.com>
2024-10-02 11:29:57 +0200

    ServiceDiscoveryAPI impl related stuffs part3

--- common-utils/src/main/java/eu/arrowhead/common/http/HttpUtilities.java
+++ common-utils/src/main/java/eu/arrowhead/common/http/HttpUtilities.java
@@ -160,23 +160,23 @@ public final class HttpUtilities {
 
 		return createURI(scheme, host, port, query, path);
 	}
-	
+
 	//-------------------------------------------------------------------------------------------------
 	public static String acquireName(final HttpServletRequest request, final String origin) throws InvalidParameterException {
-		
+
 		if (request == null) {
 			throw new InvalidParameterException("Request is null", origin);
 		}
-		
+
 		final Object nameObject = request.getAttribute(Constants.HTTP_ATTR_ARROWHEAD_AUTHENTICATED_SYSTEM);
-		
+
 		if (nameObject == null) {
 			throw new InvalidParameterException("Name is missing", origin);
 		}
-		
+
 		return nameObject.toString();
 	}
-	
+
 	//=================================================================================================
 	// assistant methods
 
