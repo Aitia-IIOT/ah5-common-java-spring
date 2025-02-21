004d39b14027d6c4e03cc804a8077de466f23e26
borditamas <borditamas@gmail.com>
2025-02-21 19:00:32 +0100

    orchestration related changes

--- data-transfer-objects/src/main/java/eu/arrowhead/dto/OrchestrationPushTriggerDTO.java
+++ data-transfer-objects/src/main/java/eu/arrowhead/dto/OrchestrationPushTriggerDTO.java
@@ -3,7 +3,7 @@ package eu.arrowhead.dto;
 import java.util.List;
 
 public record OrchestrationPushTriggerDTO(
-		List<String> tartgetSystems,
+		List<String> targetSystems,
 		List<String> subscriptionIds) {
 
 }
