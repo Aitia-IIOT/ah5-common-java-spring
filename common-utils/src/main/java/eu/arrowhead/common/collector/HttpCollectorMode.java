09b16fe4075e92db6182039eb5bedf79bdc925e0
Rajmund Bocsi <rbocsi@aitia.ai>
2025-02-27 16:16:21 +0100

    checkstyle

--- common-utils/src/main/java/eu/arrowhead/common/collector/HttpCollectorMode.java
+++ common-utils/src/main/java/eu/arrowhead/common/collector/HttpCollectorMode.java
@@ -2,9 +2,9 @@ package eu.arrowhead.common.collector;
 
 public enum HttpCollectorMode {
 	SR_ONLY, SR_AND_ORCH;
-	
+
 	//=================================================================================================
 	// members
-	
+
 	public static final String SR_AND_ORCH_VALUE = "SR_AND_ORCH"; // right side must be a constant expression
 }
