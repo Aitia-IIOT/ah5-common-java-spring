d4c779d9428188212703a5dab80e17ea04e7127a
Rajmund Bocsi <rbocsi@aitia.ai>
2025-03-07 10:56:10 +0100

    a previous fix causes problem, so I fixed the fix.

--- common-utils/src/main/java/eu/arrowhead/common/http/filter/authentication/AuthenticationPolicy.java
+++ common-utils/src/main/java/eu/arrowhead/common/http/filter/authentication/AuthenticationPolicy.java
@@ -7,7 +7,9 @@ public enum AuthenticationPolicy {
 	// members
 
 	public static final String CERTIFICATE_VALUE = "CERTIFICATE"; // right side must be a constant expression
-	public static final String INTERNAL_VALUE = "INTERNAL"; // right side must be a constant expression
 	public static final String OUTSOURCED_VALUE = "OUTSOURCED"; // right side must be a constant expression
+
+	// it is important to keep this lower-case (because @ConditionalOnExpression in CommonBeanConfig class does case sensitive)
+	public static final String INTERNAL_VALUE = "internal"; // right side must be a constant expression
 
 }
