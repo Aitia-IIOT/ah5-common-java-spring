81536c1ce31cc7f71e31ec4cc65c1e31fb6e929a
Rajmund Bocsi <rbocsi@aitia.ai>
2025-06-04 11:56:22 +0200

    minor fix

--- data-transfer-objects/src/main/java/eu/arrowhead/dto/OrchestrationServiceRequirementDTO.java
+++ data-transfer-objects/src/main/java/eu/arrowhead/dto/OrchestrationServiceRequirementDTO.java
@@ -3,6 +3,10 @@ package eu.arrowhead.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
 public record OrchestrationServiceRequirementDTO(
 		String serviceDefinition,
 		List<String> operations,
