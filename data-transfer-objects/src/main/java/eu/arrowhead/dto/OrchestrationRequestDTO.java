f0f7494f068f261ab9599e1ffefd0f1e63d2b020
Rajmund Bocsi <rbocsi@aitia.ai>
2025-06-04 11:43:25 +0200

    minor fix

--- data-transfer-objects/src/main/java/eu/arrowhead/dto/OrchestrationRequestDTO.java
+++ data-transfer-objects/src/main/java/eu/arrowhead/dto/OrchestrationRequestDTO.java
@@ -3,6 +3,10 @@ package eu.arrowhead.dto;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
 public record OrchestrationRequestDTO(
 		OrchestrationServiceRequirementDTO serviceRequirement,
 		Map<String, Boolean> orchestrationFlags,
