5adf79247cabef7351f76bc96176bc4ac4c23d83
Rajmund Bocsi <rbocsi@aitia.ai>
2025-07-25 16:19:26 +0200

    additional unit tests refactored unit tests some bugfix

--- common-utils/src/main/java/eu/arrowhead/common/http/HttpUtilities.java
+++ common-utils/src/main/java/eu/arrowhead/common/http/HttpUtilities.java
@@ -26,6 +26,7 @@ import eu.arrowhead.common.exception.InvalidParameterException;
 import eu.arrowhead.common.exception.LockedException;
 import eu.arrowhead.common.exception.TimeoutException;
 import eu.arrowhead.dto.ErrorMessageDTO;
+import eu.arrowhead.dto.enums.ExceptionType;
 import jakarta.servlet.http.HttpServletRequest;
 
 public final class HttpUtilities {
@@ -40,35 +41,12 @@ public final class HttpUtilities {
 
 	//-------------------------------------------------------------------------------------------------
 	public static HttpStatus calculateHttpStatusFromArrowheadException(final ArrowheadException ex) {
-		Assert.notNull(ex, "Exception is null.");
+		Assert.notNull(ex, "Exception is null");
 
-		HttpStatus status = HttpStatus.resolve(ex.getExceptionType().getErrorCode());
+		final ExceptionType exceptionType = ex.getExceptionType() != null ? ex.getExceptionType() : ExceptionType.INTERNAL_SERVER_ERROR;
+		HttpStatus status = HttpStatus.resolve(exceptionType.getErrorCode());
 		if (status == null) {
-			switch (ex.getExceptionType()) {
-			case AUTH:
-				status = HttpStatus.UNAUTHORIZED;
-				break;
-			case FORBIDDEN:
-				status = HttpStatus.FORBIDDEN;
-				break;
-			case INVALID_PARAMETER:
-				status = HttpStatus.BAD_REQUEST;
-				break;
-			case DATA_NOT_FOUND:
-				status = HttpStatus.NOT_FOUND;
-				break;
-			case EXTERNAL_SERVER_ERROR:
-				status = HttpStatus.SERVICE_UNAVAILABLE;
-				break;
-			case TIMEOUT:
-				status = HttpStatus.REQUEST_TIMEOUT;
-				break;
-			case LOCKED:
-				status = HttpStatus.LOCKED;
-				break;
-			default:
-				status = HttpStatus.INTERNAL_SERVER_ERROR;
-			}
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
 		}
 
 		return status;
@@ -81,7 +59,7 @@ public final class HttpUtilities {
 
 	//-------------------------------------------------------------------------------------------------
 	public static ArrowheadException createExceptionFromErrorMessageDTO(final ErrorMessageDTO dto) {
-		Assert.notNull(dto, "Error message object is null.");
+		Assert.notNull(dto, "Error message object is null");
 
 		if (dto.exceptionType() == null) {
 			return new ArrowheadException(dto.errorMessage(), dto.origin());
