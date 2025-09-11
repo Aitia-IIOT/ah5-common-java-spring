86c0f35f864befa7b54336b1fb87db875167b9bc
Rajmund Bocsi <rbocsi@aitia.ai>
2025-09-11 16:11:07 +0200

    fixing an input type in ArrowheadFilter

--- common-utils/src/test/java/eu/arrowhead/common/http/filter/authentication/OutsourcedFilterTestHelper.java
+++ common-utils/src/test/java/eu/arrowhead/common/http/filter/authentication/OutsourcedFilterTestHelper.java
@@ -19,7 +19,7 @@ package eu.arrowhead.common.http.filter.authentication;
 import java.io.IOException;
 
 import eu.arrowhead.common.exception.ArrowheadException;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 
 // we test this derived class instead of the original one
 public class OutsourcedFilterTestHelper extends OutsourcedFilter {
@@ -30,7 +30,7 @@ public class OutsourcedFilterTestHelper extends OutsourcedFilter {
 	//-------------------------------------------------------------------------------------------------
 	// this method just re-throws the input exception which is easier to test than intercept the error response somehow
 	@Override
-	protected void handleException(final ArrowheadException ex, final ServletResponse response) throws IOException {
+	protected void handleException(final ArrowheadException ex, final HttpServletResponse response) throws IOException {
 		throw ex;
 	}
 }
